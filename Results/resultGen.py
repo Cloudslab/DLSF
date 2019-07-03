db213b86cbfa4e22d8d2bfb9450cc9aed3cc8aac
shreshthtuli <34661516+shreshthtuli@users.noreply.github.com>
2019-07-03 18:13:09 +1000

    Update resultGen.py

--- Results/resultGen.py
+++ Results/resultGen.py
@@ -4,3 +4,23 @@ PATH = "../Models/"
 
 Models = ['FCN-LR-MMT']
 
+IntervalEnergy = {}
+TotalEnergy = {}
+NumVmsEnded = {}
+AvgResponseTime = {}
+TotalResponseTime = {}
+AvgMigrationTime = {}
+TotalMigrationTime = {}
+AvgCompletionTime = {}
+TotalCompletionTime = {}
+IntervalCost = {}
+TotalCost = {}
+IntervalSLA = {}
+TotalSLA = {}
+IntervalVmsMigrated = {}
+TotalVmsMigrated = {}
+
+for model in Models:
+	IE = []; TE = []; NVE = []; ART = []; TRT = []; AMT = []; TMT = []
+	ACT = []; TCT = []; IC = []; TC = []; ISLA = []; TSLA = []; IVM = []; TVM = []
+	
